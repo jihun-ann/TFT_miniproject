63f7fa2d2f67edf1944ef1239f85599f76c7f35b
jihun-ann <ajh722@naver.com>
2023-05-18 21:00:57 +0900

    Initial commit

---
+++ src/main/java/com/springboot/tft_miniproject/TftMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.tft_miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftMiniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TftMiniprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/tft_miniproject/TftMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tft_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftMiniprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
